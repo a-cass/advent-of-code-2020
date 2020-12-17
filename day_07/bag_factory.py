72e194f457a8f97c902d3c0688d9099ce437f08d
a-cass <58216420+a-cass@users.noreply.github.com>
2020-12-17 09:21:57 +0000

    added documentation to day 7 bag_factory.py

--- day_07/bag_factory.py
+++ day_07/bag_factory.py
@@ -1,13 +1,35 @@
 """
 BAG FACTORY
+
+Module for parsing of bag rules and creation of Bag objects.
+
 """
 import os
 import re
 
+# Default location for rules file
 RULES_SRC = './bags.txt'
 
 
 def parse_rules(bag_list=RULES_SRC):
+    """
+    Parse file of bag rules to dictionary.
+
+    Parameters
+    ----------
+    bag_list: str, optional
+        Path to source file. The default path is determined from the
+        module constant `RULES_SRC`.
+
+    Returns
+    -------
+    rules: dict(str:dict(str:int))
+        Dictionary of bag colours and corresponding rules for each bag
+        identified in `bag_list`. Keys are bag colours; values are
+        acceptance rules for each bag. Acceptance rules are dictionaries
+        of the form colour:max allowed.
+
+    """
     rules = {}
     with open(bag_list) as f:
         line = f.readline()
@@ -26,10 +48,36 @@ def parse_rules(bag_list=RULES_SRC):
     return rules
 
 
+# On initialisation set bag rules to result of parsing the file RULES_SRC
 BAG_RULES = parse_rules()
 
 
 def get_bag(colour, rules=BAG_RULES):
+    """
+    Creates a Bag object using the colour and rules specified.
+
+    Parameters
+    ----------
+    colour: str
+        Colour of bag to construct
+    rules: dict(str:dict(str:int)), optional
+        Dictionary of bag colours and corresponding rules. Keys are bag
+        colours; values are acceptance rules for each bag. Acceptance
+        rules are dictionaries of the form colour:max allowed.The default
+        value uses rules from module constant `BAG_RULES`, determined on
+        module initialisation.
+
+    Returns
+    -------
+    Bag
+        `Bag` object with associated colour rules.
+
+    Raises
+    ------
+    KeyError
+        If specified colour is not found in `rules` keys.
+
+    """
     try:
         bag_accepts = rules[colour]
         return Bag(colour, bag_accepts)
@@ -40,19 +88,66 @@ def get_bag(colour, rules=BAG_RULES):
 
 def iter_bags(rules=BAG_RULES):
     """
+    Iterate through all available bags in the rules dictionary.
+
+    At each iteration a Bag object is constructed based on the
+    corresponding rules.
+
+    Parameters
+    ----------
+    rules: dict(str:dict(str:int)), optional
+        Dictionary of bag colours and corresponding rules. Keys are bag
+        colours; values are acceptance rules for each bag. Acceptance
+        rules are dictionaries of the form colour:max allowed.The default
+        value uses rules from module constant `BAG_RULES`, determined on
+        module initialisation.
+
+    Yields
+    ------
+    Bag
+        `Bag` object with associated colour rules.
+
     """
     for bag_colour, bag_accepts in BAG_RULES.items():
-        bag = Bag(bag_colour, bag_accepts)
-        yield bag
+        yield Bag(bag_colour, bag_accepts)
 
 
 class Bag(object):
+    """
+    A bag object of a particular colour.
+
+    Bags may have other bags inside them and have rules determining
+    the colours and amount of these inner bags.
+
+    Parameters
+    __________
+    colour: str
+        Bag colour.
+    accepts: dict(str:int))
+        Acceptance rules for inner bags. Keys are accepted colours, values are
+        the maximum allowed amount of each bag.
+
+    """
     def __init__(self, colour, accepts=None):
         self.colour = colour
         self.accepts = accepts
         self.contents = {k: 0 for k in accepts.keys()}
 
     def total_bags(self):
+        """
+        Total number of inner bags allowed inside this bag.
+
+        As inner bags may themselves have inner bags this function is
+        recursive. The total for this bag is made up of the contents of
+        accepted inner bags multiplied by the maximum allowed amount of
+        each bag.
+
+        Returns
+        -------
+        total: int
+            Total amount of inner bags.
+
+        """
         total = 0
         for accept_colour, accept_count in self.accepts.items():
             total += accept_count
@@ -62,6 +157,32 @@ class Bag(object):
         return total
 
     def can_accept(self, colour, colours_seen=None):
+        """
+        Determines if this bag can contain a bag of the specified colour.
+
+        A check is performed to see if this bag can directly contain the
+        specified bag i.e. it is an accepted inner bag. If it cannot the
+        list of accepted bags are then searched recursively.
+
+        The `colours_seen` argument is used to keep track of the inner
+        bags already checked and avoid infinite checks.
+
+        Parameters
+        ------
+        colour: str
+            Colour of bag to check.
+        colours_seen: list of str, optional
+            List of inner bag colours already checked; default is None.
+            This parameter is used when traversing inner bags. Bags
+            already checked are not checked again.
+
+        Returns
+        -------
+        accepted: bool
+            True if this bag can contain the specified bag either directly
+            or through inner bags.
+
+        """
         if colour in self.accepts.keys():
             return True
         else:
@@ -87,3 +208,4 @@ if __name__ == '__main__':
     bag = get_bag('shiny gold')
     print(bag.colour)
     print(bag.accepts)
+    print(globals())
